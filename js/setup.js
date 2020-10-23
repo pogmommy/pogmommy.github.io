ea1342fe37e901e56735cef28a33cb6286673662
MPG13 (Micah Gomez) <micahplacido@gmail.com>
2020-10-22 20:29:18 -0600

    layout updates, for the most part. Consolidates stuffs

--- js/setup.js
+++ js/setup.js
@@ -1,3 +1,6 @@
 $(function () {
+	//$("#footer-placeholder").hide();
 	$("#footer-placeholder").load("/inject/footer.html");
+	//$("#footer-placeholder").fadeIn("fast");
+	$("#footer-placeholder").fadeOut(2000);
 });
