87b8a95d7f57fed96115d4a9930f1d56e77820a7
Micah Gomez (MPG13) <micahplacido@gmail.com>
2022-05-12 16:40:08 -0600

    updated linked to new MUF (micah universal format))

--- js/setup.js
+++ js/setup.js
@@ -4,3 +4,32 @@ $(function () {
 	//$("#footer-placeholder").fadeIn("fast");
 	$("#overlay").fadeOut(2000);
 });
+
+var domain = ""
+if (window !== window.parent){
+	domain = "http://pogmom.me/";
+} else {
+	domain = "http://micahpgomez.com/";
+}
+console.log(domain)
+
+function mRdr(a){
+	console.log("MicahRedirect!");
+	console.log(arguments[0]);
+	var newurl = domain + arguments[0];
+	console.log(newurl);
+	//parent.location.src = newurl;
+	window.top.location.href = newurl;
+}
+
+function mExt(a){
+	console.log("MicahExternal!");
+	console.log(arguments[0]);
+	window.open(arguments[0]);
+}
+
+function mDL(a){
+	console.log("MicahDownload!");
+	console.log(arguments[0]);
+	window.top.location.href = arguments[0];
+}
